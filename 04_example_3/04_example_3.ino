82c15eb9bcbd72193f5e00157a16eb442456e7ea
jin3079 <hajinseo@kookmin.ac.kr>
2020-10-05 13:01:28 +0900

    04_example_3 ver.2

--- 04_example_3/04_example_3.ino
+++ 04_example_3/04_example_3.ino
@@ -16,12 +16,13 @@ void setup() {
 void loop() {
   Serial.println(++count);
   toggle = toggle_state(toggle); // toggle LED value.
-  digitalWrite(PIN_LED, HIGH); // update LED status.
+  digitalWrite(PIN_LED, toggle); // update LED status.
   delay(1000); // wait for 1,000 milliseconds
-  digitalWrite(PIN_LED, LOW);
-  delay(1000);
 }
 
 int toggle_state(int toggle) {
-  return toggle;
+  if (toggle == 0)
+    return toggle = 1;
+  else
+    return toggle = 0;
 }
